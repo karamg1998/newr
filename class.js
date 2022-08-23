f234e0c7c4905ab47ec32d81bfaa9a1bb63002b4
karamg1998 <111856262+karamg1998@users.noreply.github.com>
2022-08-23 14:20:15 +0530

    class commit

---
+++ class.js
@@ -0,0 +1,31 @@
+class student
+
+{
+
+  constructor(first_name,last_name,roll_number,Sex,date_of_birth)
+
+  {
+
+    this.first_name=first_name;
+
+   this.last_name=last_name;
+
+   this.roll_number=roll_number;
+
+    this.Sex=Sex;
+
+    this.date_of_birth=new Date(date_of_birth);
+
+  }
+ 
+  get_birth_year()
+  {
+   return this.date_of_birth.getFullYear();
+  }
+  
+}
+
+var student1=new student('karamveer',' garathi',25,'male','4-3-1998');
+var student2=new student('ved',' jangid',26,'male','10-08-2000');
+
+console.log(student1.get_birth_year());
